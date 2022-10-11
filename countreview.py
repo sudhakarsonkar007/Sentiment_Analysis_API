4d64eeddf1df38a3f8fbb6bfa20a601972324f3c
Anuragmaurya18 <112384551+Anuragmaurya18@users.noreply.github.com>
2022-10-11 16:06:00 +0530

    Update countreview.py

--- countreview.py
+++ countreview.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sentimentscore import get_sentiment_score
 from formatoutput import formatoutput
+from getreviews import get_review
 
 
 def count_text_review(data):
@@ -39,3 +40,26 @@ def count_excel_review(file):
                  
     count=formatoutput(total_review,pos_count,neg_count,neu_count)
     return count
+
+def count_product_review(url):
+    file = get_review(url)
+    file.columns=['Reviews']
+    list=file['Reviews'].values.tolist()
+    
+    total_review=0
+    pos_count=0
+    neg_count=0
+    neu_count=0
+    for text in list:
+        score=get_sentiment_score(text)
+        total_review += 1
+        if score[0][0] > 0:
+            pos_count += 1
+        elif score[0][0] < 0:
+            neg_count += 1
+        else:
+            neu_count += 1  
+                 
+    count=formatoutput(total_review,pos_count,neg_count,neu_count)
+    return count
+
