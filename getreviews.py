6c2b8b795a509b3f88b5f952811dd7ed56bc7da0
Anuragmaurya18 <anurag.maurya@geminisolutions.com>
2022-10-13 13:17:07 +0530

    changes

--- getreviews.py
+++ getreviews.py
@@ -2,33 +2,7 @@ import pandas as pd
 import numpy as np
 import requests 
 from bs4 import BeautifulSoup
-
-
-headers = {
-    'authority': 'www.amazon.in',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'device-memory': '8',
-    'downlink': '10',
-    'dpr': '0.8',
-    'ect': '4g',
-    'rtt': '0',
-    'sec-ch-device-memory': '8',
-    'sec-ch-dpr': '0.8',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-ch-viewport-width': '2400',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'service-worker-navigation-preload': 'true',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
-    'viewport-width': '2400',
-}
+from header import headers
 
 
 def Get_Soup(url):
@@ -55,7 +29,7 @@ def Get_Url(productUrl):
     changeInUrl = shortProductUrl.replace("/dp/", "/product-reviews/")
     changeInUrl = shortProductUrl.replace("/gp/", "/product-reviews/")
     finalUrl = changeInUrl + \
-    "ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber="
+    "ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&reviewerType=avp_only_reviews&sortBy=recent&pageNumber=1&filterByStar=one_star&pageNumber="
     return finalUrl
 
 # loop through :x many pages, or until the css selector found only on the last page is found (when the next page button is greyed) 
